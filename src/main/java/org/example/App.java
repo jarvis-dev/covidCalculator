1d42624ab8da5da867c200a0ffe3751e21cce53a
saki <saki.osive@gmail.com>
2020-07-17 04:27:58 +0530

    Added comments

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+
+        double daysElapsed;
+        double casesForToday;
+        double totalCases;
+        double increaseInCases;
+
+
+
+        Scanner objInput = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter the number of days elapsed since March 15");
+
+
+
+        daysElapsed = objInput.nextDouble();  // Read user input
+        increaseInCases=8*daysElapsed-300;   // Predicted rate of increase in cases w.r.t. time
+        System.out.printf("The rate of increase in cases for today is :" +Math.round(increaseInCases));
+        System.out.println();
+
+        casesForToday=4*Math.pow(daysElapsed,2) - 300*daysElapsed + 7115; // Predicted increase in cases
+
+        System.out.println("The predicted number of cases for today are :" +Math.round(casesForToday));  // Output user input
+        double y = daysElapsed-1;
+        double z = 4*Math.pow(y,2) - 300*y + 7115; // Total predicted cases
+        double x = casesForToday/z ;
+        System.out.println("The predicted percent increase in cases from yesterday is :" + x + "%");
+        System.out.println();
+
+        totalCases=4*Math.pow(daysElapsed,3)/3-300*Math.pow(daysElapsed,2)/2 + 7115*daysElapsed;
+        System.out.printf("The total predicted number of infected people till now are :" +Math.round(totalCases));
+
+
+
+
+
+
+
+    }
+
+
+
+}
